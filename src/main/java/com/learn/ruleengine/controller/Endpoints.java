786922cb8895a7ba23ad307cbddf4166f6a140db
Sunil M <msunil92@gmail.com>
2021-07-26 09:03:05 +0530

    drool rule engine

--- src/main/java/com/learn/ruleengine/controller/Endpoints.java
+++ src/main/java/com/learn/ruleengine/controller/Endpoints.java
@@ -2,6 +2,7 @@ package com.learn.ruleengine.controller;
 
 import com.learn.ruleengine.model.Customer;
 import com.learn.ruleengine.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class Endpoints {
 
     @Autowired
@@ -29,33 +31,45 @@ public class Endpoints {
 
     @GetMapping(value = "customer" , produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer getcustomer() {
+        long startTime = System.currentTimeMillis();
         Customer customer = customerService.getCustomerExcel();
         System.out.println(customer);
         System.out.println(customer.getOffers());
+        long endTime = System.currentTimeMillis();
+        log.info("Excel Rule Engine :: " + (endTime - startTime));
         return customer;
     }
 
     @GetMapping(value = "customer_drl" , produces = MediaType.APPLICATION_JSON_VALUE)
     public Customer getcustomer_drl() {
+        long startTime = System.currentTimeMillis();
         Customer customer = customerService.getCustomerDrl();
         System.out.println(customer);
         System.out.println(customer.getOffers());
+        long endTime = System.currentTimeMillis();
+        log.info("Text File Rule Engine :: " + (endTime - startTime));
         return customer;
     }
 
     @PostMapping(value = "customer" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Customer updateCustomer(@RequestBody Customer requestCustomer) {
+        long startTime = System.currentTimeMillis();
         Customer customer = customerService.updateCustomerFromExcelRules(requestCustomer);
         System.out.println(customer);
         System.out.println(customer.getOffers());
+        long endTime = System.currentTimeMillis();
+        log.info("Excel Rule Engine :: " + (endTime - startTime));
         return customer;
     }
 
     @PostMapping(value = "customer_drl" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Customer updateCustomerWithDrool(@RequestBody Customer requestCustomer) {
+        long startTime = System.currentTimeMillis();
         Customer customer = customerService.updateCustomerFromDrlRules(requestCustomer);
         System.out.println(customer);
         System.out.println(customer.getOffers());
+        long endTime = System.currentTimeMillis();
+        log.info("Text File Rule Engine :: " + (endTime - startTime));
         return customer;
     }
 }
